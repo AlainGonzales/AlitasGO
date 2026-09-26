bf6ef470b256d0b9524e5c0734d977e83800871e
Daniel27-coder <dani_alex_lazaro@hotmail.com>
2026-09-26 03:04:07 -0500

    Nivel 1: Interfaz visual del salón finalizada

---
+++ AlitasGo.Web/Controllers/PedidosController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AlitasGo.Web.Controllers
+{
+    public class PedidosController : Controller
+    {
+        // Acción temporal solo para que puedas ver tu vista del Nivel 1
+        public IActionResult FrmPedidosSalon()
+        {
+            return View();
+        }
+    }
+}
